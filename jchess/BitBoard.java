9e3c7b2cfe3c0926f513eb959d6b6933fb63d5ec
kris10ansn <kristian.nessa@protonmail.com>
2025-07-03 15:03:02 +0200

    use L when bitshifting to make sure no errors are created

--- jchess/BitBoard.java
+++ jchess/BitBoard.java
@@ -3,13 +3,13 @@ package jchess;
 public class BitBoard {
 
     public static final long RANK_8 = 0b11111111;
-    public static final long RANK_7 = RANK_8 << 8;
-    public static final long RANK_6 = RANK_7 << 8;
-    public static final long RANK_5 = RANK_6 << 8;
-    public static final long RANK_4 = RANK_5 << 8;
-    public static final long RANK_3 = RANK_4 << 8;
-    public static final long RANK_2 = RANK_3 << 8;
-    public static final long RANK_1 = RANK_2 << 8;
+    public static final long RANK_7 = RANK_8 << 8L;
+    public static final long RANK_6 = RANK_7 << 8L;
+    public static final long RANK_5 = RANK_6 << 8L;
+    public static final long RANK_4 = RANK_5 << 8L;
+    public static final long RANK_3 = RANK_4 << 8L;
+    public static final long RANK_2 = RANK_3 << 8L;
+    public static final long RANK_1 = RANK_2 << 8L;
 
     public static final long FILE_1 = 0x0101010101010101L;
     public static final long FILE_2 = FILE_1 << 1L;
