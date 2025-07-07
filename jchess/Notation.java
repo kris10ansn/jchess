1b6b9afa3f3ad9a9e5fabafdb2492fbea2280128
kris10ansn <kristian.nessa@protonmail.com>
2025-07-07 13:04:06 +0200

    add toNotation method for converting index to notation

--- jchess/Notation.java
+++ jchess/Notation.java
@@ -15,4 +15,11 @@ public class Notation {
     public static int toIndex(int file, int rank) {
         return rank * 8 + file;
     }
+
+    public static String toNotation(int index) {
+        final int file = index % 8;
+        final int rank = index / 8;
+        
+        return ('a' + file) + String.valueOf(rank);
+    }
 }
