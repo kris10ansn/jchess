d4f483a765b2abb3e996c910861a25792715c91c
kris10ansn <kristian.nessa@protonmail.com>
2025-07-22 00:49:06 +0200

    simplify window creation

--- JChess.java
+++ JChess.java
@@ -1,7 +1,4 @@
 
-import java.awt.Dimension;
-import java.awt.GraphicsConfiguration;
-import java.awt.Rectangle;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import jchess.Board;
@@ -25,22 +22,13 @@ public class JChess {
 
     private static void createWindow() {
         JFrame frame = new JFrame("JChess");
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1024, 1024);
 
-        var p = new ChessBoardPanel(board);
-        frame.add(p);
+        frame.add(new ChessBoardPanel(board));
         frame.pack();
 
-        GraphicsConfiguration gc = frame.getGraphicsConfiguration();
-        Rectangle bounds = gc.getBounds();
-        Dimension frameSize = frame.getPreferredSize();
-
-        frame.setLocation(
-                (int) (bounds.width / 2 - frameSize.width / 2),
-                (int) (bounds.height / 2 - frameSize.height / 2)
-        );
+        // Center window on screen
+        frame.setLocationRelativeTo(null);
 
         frame.setResizable(false);
         frame.setVisible(true);
